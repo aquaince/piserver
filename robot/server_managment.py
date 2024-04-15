210ad1033c4657e25c37eb587cb2cc10360912a2
Uglypr1nces <shpatavdiu4@gmail.com>
2024-04-15 14:20:21 +0200

    added movement

--- robot/server_managment.py
+++ robot/server_managment.py
@@ -1,5 +1,7 @@
 import socket
 import threading
+from robot.robot_actions import Robot
+import pyttsx3
 
 class Server:
     def __init__(self, host, port, header=64, format="utf-8", disconnect_cmd="!bye"):
@@ -11,7 +13,13 @@ class Server:
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_socket.bind((self.host, self.port))
 
+        #robot
+        self.servoangle = 3
+        self.pan_angle = 0
+        self.tilt_angle = 0
+
     def start(self):
+        self.robot = Robot()
         self.server_socket.listen()
         print(f"Server started on {self.host}:{self.port}...")
         while True:
@@ -30,5 +38,37 @@ class Server:
                 print(f"Received message: {msg}")
                 if msg == self.disconnect_cmd:
                     connected = False
-                # Handle other commands...
+                if msg == "forward":
+                    Robot.move_forward()
+                if msg == "backward":
+                    Robot.move_backward()
+
+                if msg == "right":
+                    self.servoangle += 3
+
+                if msg == "left":
+                    self.servoangle -= 3
+
+                if msg == "camera_up":
+                    self.tilt_angle += 4
+                if msg == "camera_down":
+                    self.tilt_angle -= 4
+                if msg == "camera_left":
+                    self.pan_angle += 4
+                if msg == "camera_right":
+                    self.pan_angle -= 4
+
+                if msg == "over_sound":
+                    connected = False
+                    subprocess.call("shutdown.sh")
+
+                if msg == "alarm_sound":
+                    alarm_sound.play()
+                if msg[:4] == "word":
+                    print(msg[4:])
+                    engine.say(msg[4:])
+                    engine.runAndWait()
+                
+                Robot.set_servo_angle(self.servoangle)
+                Robot.set_camera_angles(self.pan_angle, self.tilt_angle)
         conn.close()
